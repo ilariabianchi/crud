a4f1c583f3df845ca9867f2632b4c93a3dc41db5
ilaria <ilariabianchi2010@gmail.com>
2026-02-10 08:51:07 +0100

    elenco operazioni + funzione cerca

---
+++ crud.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+//parametri: vettore, dimensione, elemento da cercare
+void cerca(int v[], int d, int n){
+	
+	for(int i=0; i<d; i++){
+		if(v[i]==n){
+			cout<<"l'elemento si trova in posizione: "<<i<<endl;
+		}
+		else{
+			cout<<"-1";
+		}
+	}
+}
+
+
+void inserisci(int v[], int &d){
+	
+}
+
+void visualizza(int v[], int d){
+	
+	
+}
+
+void modifica(int v[], int d){
+	
+}
+
+void cancella(int v[], int d){
+	
+}
+
+int main(int argc, char** argv) {
+	
+	do{
+		int v[];
+		int dim=0, n=0;
+		int opzione;
+		cout<<"1 - inserimento\n 2 - visualizzazione\n 3 - modifica\n 4 - cancellazione\n";
+		cout<<"inserisci l'opzione: ";
+		cin>>opzione;
+		
+		switch(opzione):
+			case 1: 
+			//inserimento
+			case 2:
+			//visualizzazione
+			case 3: 
+			//modifica
+			case 4:
+			//cancellazione			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}while(opzione!=0);
+}
